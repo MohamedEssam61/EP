9c173bf06dce90afa4264a2f95e058eb04d7c640
MohamedEssam61 <50160450+MohamedEssam61@users.noreply.github.com>
2023-01-01 15:18:12 +0200

    fix user pos field error

--- pos_user_restrict/models/res_users.py
+++ pos_user_restrict/models/res_users.py
@@ -16,6 +16,5 @@ class ResUsers(models.Model):
         res = super(ResUsers, self).write(values)
         if 'pos_config_ids' in values:
             self.env['ir.model.access'].call_cache_clearing_methods()
-            self.env['ir.rule'].clear_caches()
-            self.has_group.clear_cache(self)
+            self.clear_caches()
         return res
